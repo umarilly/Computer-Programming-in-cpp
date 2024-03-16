604952e0d9dff792aaa501e92ddd57587adeb27b
Muhammad Umar <muhammadumar695@gmail.com>
2024-03-16 10:14:18 +0500

    Cout Pattern Program is Added

---
+++ 1. Introduction to Computer Programming in C++/2. Basic Cout Pattern Programs/Triangle Pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	cout << "     /\\      " << endl;
+	cout << "    /  \\     " << endl;
+	cout << "   /    \\    " << endl;
+	cout << "  /      \\   " << endl;
+	cout << "  --------  ";
+	_getch();
+	return 0;
+}
