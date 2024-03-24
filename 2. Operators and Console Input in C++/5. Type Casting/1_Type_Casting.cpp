f4d3c3c6a6ca4214e2f856520ea1b8417ef93be9
Muhammad Umar <muhammadumar695@gmail.com>
2024-03-24 11:02:40 +0500

    Type casting with details and practical example is added

---
+++ 2. Operators and Console Input in C++/5. Type Casting/1_Type_Casting.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    // Implicit conversion: 'a' is automatically converted to double for division
+    double b = a / 3;
+
+    cout << "The Sum is : " << a << endl;
+    cout << "The Average is : " << b << endl;
+
+    // Explicit casting using static_cast to convert 'a' to double before division
+    b = static_cast<double>(a) / 3;
+    cout << "The Average after static_cast is : " << b << endl;
+
+    /*
+    Following casting types are used with advance concepts like pointers and polymorphism, so we will see then later
+        const_cast
+        dynamic_cast
+        reinterpret_cast
+    */
+
+    return 0;
+}
