95f8e04868e6070d3254397ac4cf00c9cc03d3ac
Muhammad Umar <muhammadumar695@gmail.com>
2024-03-24 10:36:01 +0500

    The Increment and Decrement operator are solved with all post and prefix types, the programs are code and user can also consult to the theory from the Word file

---
+++ 2. Operators and Console Input in C++/4. Increment and Decrement Operators/1_Increment_Operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Increment Operator
+    int x = 5;
+    cout << "Initial value of x: " << x << endl;
+
+    // Prefix Increment
+    cout << "Prefix Increment: " << endl;
+    cout << "Before incrementing, x is: " << x << endl;
+    int y = ++x; // Prefix increment: increments x first, then assigns the incremented value to y
+    cout << "After prefix increment, x is: " << x << ", y is: " << y << endl;
+
+    // Postfix Increment
+    x = 5; // Reset x to 5
+    cout << "Postfix Increment: " << endl;
+    cout << "Before incrementing, x is: " << x << endl;
+    y = x++; // Postfix increment: assigns the current value of x to y, then increments x
+    cout << "After postfix increment, x is: " << x << ", y is: " << y << endl;
+
+    return 0;
+}
---
+++ 2. Operators and Console Input in C++/4. Increment and Decrement Operators/2_Decrement_Operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Decrement Operator
+    int a = 10;
+    cout << "\nInitial value of a: " << a << endl;
+
+    // Prefix Decrement
+    cout << "Prefix Decrement: " << endl;
+    cout << "Before decrementing, a is: " << a << endl;
+    int b = --a; // Prefix decrement: decrements a first, then assigns the decremented value to b
+    cout << "After prefix decrement, a is: " << a << ", b is: " << b << endl;
+
+    // Postfix Decrement
+    a = 10; // Reset a to 10
+    cout << "Postfix Decrement: " << endl;
+    cout << "Before decrementing, a is: " << a << endl;
+    b = a--; // Postfix decrement: assigns the current value of a to b, then decrements a
+    cout << "After postfix decrement, a is: " << a << ", b is: " << b << endl;
+
+    return 0;
+}
