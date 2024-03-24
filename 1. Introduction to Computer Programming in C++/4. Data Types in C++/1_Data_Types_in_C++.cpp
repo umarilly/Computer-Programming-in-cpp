55c2a340a3309180ae7ab5e88c183aa87be09759
Muhammad Umar <muhammadumar695@gmail.com>
2024-03-24 08:27:37 +0500

    Data Types in C++ are added in the folder with practical examples and their sizes

---
+++ 1. Introduction to Computer Programming in C++/4. Data Types in C++/1_Data_Types_in_C++.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    /*  
+        All the basic data types that are used in C++ programming
+        1. int
+        2. float
+        3. void
+        4. bool
+        5. char
+        6. double
+
+        Note:
+            There are more data types that are used in c++, They are mentioned in the document of Parent folder
+    */
+
+
+    // Integer
+    int myInt = 42;
+    cout << "Integer: " << myInt << endl;
+
+    // Floating-point
+    float myFloat = 3.14f;
+    cout << "Float: " << myFloat << endl;
+
+    /* 
+        Void (no value)
+        The void data type cannot be used to declare variables because it represents the absence of a type. It can use as return type of function.
+    */
+
+    // Boolean
+    bool myBool = true;
+    cout << "Boolean: " << myBool << endl;
+
+    // Character
+    char myChar = 'A';
+    cout << "Character: " << myChar << endl;
+
+
+    // Double
+    double myDouble = 3.14159;
+    cout << "Double: " << myDouble << endl;
+
+    // Size of data types
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of bool: " << sizeof(bool) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
