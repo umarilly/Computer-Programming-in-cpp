b3ad11c18654c66422bc28a461236a1632fb370c
Muhammad Umar <muhammadumar695@gmail.com>
2024-04-24 21:33:07 +0500

    I have the insertion sorting algorithm using an integer array of 6 elements. I have used two helping valriables temp and ptr, temp holds the value of [i] where as ptr holds the value of [i-1] meaning that, ptr is the left and temp is the right. Each value in temp is examine with the ptr value if, it is smaller then it is swapped.

--- 4. Arrays in C++/4. Sorting in Arrays/2. Insertion Sorting/1_Simple_Example.cpp
+++ 4. Arrays in C++/4. Sorting in Arrays/2. Insertion Sorting/1_Simple_Example.cpp
@@ -1,28 +1,26 @@
-
 #include <iostream>
 #include <conio.h>
 using namespace std;
 int main()
 {
-    int abc[5] = {2, 4, 1, 5, 3};
+    int arr[6] = {2,8,5,3,9,4};
     int i;
     int ptr, temp;
 
-    for (i = 1; i < 5; i++)
-    {
-        temp = abc[i];
-        ptr = i - 1;
-        while ((temp < abc[ptr]) && (ptr != -1))
-        {
-            abc[ptr + 1] = abc[ptr];
-            ptr = ptr - 1;
+    for (i = 1; i < 6; i++) {
+        temp = arr[i]; 
+        ptr = i - 1; 
+        while ((ptr >= 0) && (temp < arr[ptr])) {
+            arr[ptr + 1] = arr[ptr]; 
+
+            ptr = ptr - 1; 
         }
-        abc[ptr + 1] = temp;
+        arr[ptr + 1] = temp;
     }
 
     cout << "The Sorted array is : ";
-    for (i = 0; i < 5; i++)
-        cout << abc[i] << " ";
+    for (i = 0; i < 6; i++)
+        cout << arr[i] << " ";
 
     _getch();
     return 0;
