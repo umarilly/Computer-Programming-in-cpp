bea1e4950484d59cf9ceb4280b1962d5afa3b77c
Muhammad Umar <muhammadumar695@gmail.com>
2024-04-26 23:28:15 +0500

    The output of the sorting algoritm was clear, there was issue of spacing. I have adjusted all the spaces and not the results are clearly displaying.

--- 4. Arrays in C++/4. Sorting in Arrays/4. Merge Sort/Merge_Sort.cpp
+++ 4. Arrays in C++/4. Sorting in Arrays/4. Merge Sort/Merge_Sort.cpp
@@ -58,16 +58,19 @@ int main() {
     int arr[] = {12, 11, 13, 5, 6};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Given array is \n";
-    for (int i = 0; i < arr_size; i++)
+    cout << "Given array is : " << endl;
+    for (int i = 0; i < arr_size; i++) {
         cout << arr[i] << " ";
+    }
+        
     cout << endl;
 
     mergeSort(arr, 0, arr_size - 1);
 
-    cout << "\nSorted array is \n";
-    for (int i = 0; i < arr_size; i++)
+    cout << "\nSorted array is : " << endl;
+    for (int i = 0; i < arr_size; i++) {
         cout << arr[i] << " ";
+    }
     cout << endl;
 
     _getch();
