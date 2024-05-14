f4b7b43cbea0ac449a172fc96d329d14017d2c02
Muhammad Umar <muhammadumar695@gmail.com>
2024-05-14 20:58:27 +0500

    Implemented a basic program to show the working of console input using functions. Additionally, them displayed that input in the main().

---
+++ 5. Functions in C++/1. Basics of Functions in C++/5_Console_Input_in_Functions.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int xyz( int index) {
+	cin >> index;
+	return index;
+}
+
+int main() {
+    
+	int a=0;
+	cout << xyz(a);
+
+	_getch();
+	return 0;
+}
