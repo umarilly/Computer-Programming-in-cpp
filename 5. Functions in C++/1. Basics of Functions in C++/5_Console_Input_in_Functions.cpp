378bac7425f13b37ead9e192fede42e2bbe3aa7b
Muhammad Umar <muhammadumar695@gmail.com>
2024-05-14 21:03:02 +0500

    To make better understandable code for the user, some user-friendly console outputs are displed on the screen. Additionally, more meaning full names are given to the varibale.

--- 5. Functions in C++/1. Basics of Functions in C++/5_Console_Input_in_Functions.cpp
+++ 5. Functions in C++/1. Basics of Functions in C++/5_Console_Input_in_Functions.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-int xyz( int index) {
-	cin >> index;
-	return index;
+int xyz( int input) {
+    cout << "Enter the input please ( Taking input inside function ) : ";
+	cin >> input;
+	return input;
 }
 
 int main() {
-    
-	int a=0;
-	cout << xyz(a);
+
+	int output = 0;
+	cout << "Displying the output ( In the main() body ) : " << xyz(output);
 
 	_getch();
 	return 0;
