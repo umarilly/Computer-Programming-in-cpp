0825018f03f9534e21834a925588810d672a23f1
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-04 19:03:43 +0500

    Implemented a function to calculate the sum and average of two numbers. The sum_avg function computes the sum and average correctly, but it uses pass-by-value, which does not update the original variables in the main function. As a result, the sum and average values are not correctly reflected in the output. Further modification is needed to ensure that the computed values are passed back to the main function correctly.

---
+++ 5. Functions in C++/2. Call or Pass by Value/5_Example.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void sum_avg (double i , double j ,double sum ,double avg) {
+	sum = i + j;
+	avg = sum / 2;
+}
+
+int main() {
+    
+	int a, b;
+	int sum = 0, avg = 0;
+
+	cout << "Enter two numbers :- \n";
+	cin >> a >> b;
+
+	sum_avg(a, b, sum, avg);
+
+	cout << "The sum is : " << sum << endl;
+	cout << "The avg is : " << avg << endl;
+
+	_getch();
+	return 0;
+}
