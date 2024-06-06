8886c85788e27d1a8753aed8e34e5d2f81e44e80
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-06 10:27:46 +0500

    Refactored the sum function to use reference parameters, allowing modifications to the original variables. The function adds 5 to each input, computes their sum, and displays the modified values. Updated the main function to reflect these changes and print the results.

---
+++ 5. Functions in C++/3. Call or Pass by Reference/1_Example.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int sum(int & x, int & y) {
+	
+    x = x + 5;
+	y = y + 5;
+	cout << "\na and b in function are \n" << x << " & " << y << endl;
+	return x + y;
+
+}
+
+int main() {
+    
+	int a = 10, b = 15;
+	int c = sum(a, b);
+	
+	cout << "\nSum is : " << c << endl;
+
+	cout << "\na and b in main are \n" << a << " & " << b << endl;
+
+	_getch();
+	return 0;
+}
