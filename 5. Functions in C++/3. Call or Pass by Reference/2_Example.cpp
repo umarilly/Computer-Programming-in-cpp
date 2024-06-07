c41b9fb0bdd1f0d0580f5773321bab8756df98a7
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-07 18:32:27 +0500

    Implemented the swap function using reference parameters to exchange values of two integers. Added output statements in the main function to display values before and after swapping for better understanding.

--- 5. Functions in C++/3. Call or Pass by Reference/2_Example.cpp
+++ 5. Functions in C++/3. Call or Pass by Reference/2_Example.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
 #include<conio.h>
+
 using namespace std;
-void swap (int & x, int & y)
-{
+
+void swap (int & x, int & y) {
+
 	int z;
 	z = x;
 	x = y;
 	y = z;
 }
-int main()
-{
+
+int main() {
+
 	int a = 10, b = 15;
 	
+    // Just for the better understanding of swapping
 	cout << "Before Swapping :- \n";
-	cout << "a  = " << a << endl;
-	cout << "b  = " << b << endl;
+	cout << "a  : " << a << endl;
+	cout << "b  : " << b << endl;
 
 	swap(a, b);
 
 	cout << "After Swapping :- \n";
-	cout << "a  = " << a << endl;
-	cout << "b  = " << b << endl;
+	cout << "a  : " << a << endl;
+	cout << "b  : " << b << endl;
 	
 	_getch();
 	return 0;
