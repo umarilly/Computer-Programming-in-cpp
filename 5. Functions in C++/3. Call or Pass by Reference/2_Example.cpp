d80fcc63bb2897500db5927c29a24106c593ef4d
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-07 18:29:48 +0500

    Implemented the program of swapping two numbers using a third temp variable, this program is implemented by passing the reference not the actual value.

---
+++ 5. Functions in C++/3. Call or Pass by Reference/2_Example.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void swap (int & x, int & y)
+{
+	int z;
+	z = x;
+	x = y;
+	y = z;
+}
+int main()
+{
+	int a = 10, b = 15;
+	
+	cout << "Before Swapping :- \n";
+	cout << "a  = " << a << endl;
+	cout << "b  = " << b << endl;
+
+	swap(a, b);
+
+	cout << "After Swapping :- \n";
+	cout << "a  = " << a << endl;
+	cout << "b  = " << b << endl;
+	
+	_getch();
+	return 0;
+}
