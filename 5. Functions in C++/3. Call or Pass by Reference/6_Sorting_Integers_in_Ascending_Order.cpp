69de65a501a9596de64f217ebc1e02645983de0d
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-12 09:47:55 +0500

    Implemented a sorting algorithm using a custom swap function to sort three integers in ascending order. The program reads three integers from the user, swaps them if needed, and displays the sorted result.

---
+++ 5. Functions in C++/3. Call or Pass by Reference/6_Sorting_Integers_in_Ascending_Order.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void swap(int & num_1, int & num_2) {
+	int temp;
+	temp = num_1;
+	num_1 = num_2;
+	num_2 = temp;
+}
+
+int main() {
+    
+	int first, second, third;
+
+	cout << "Enter three integers :- \n";
+	cin >> first >> second >> third;
+	
+	if (first > second) 
+		swap(first, second);
+	if (second > third) 
+		swap(second, third);
+	if (first > second) 
+		swap(first, second);
+	
+	cout << "The Sorted integers are  " << first << " , " << second << " , " << third << endl;
+
+	_getch();
+	return 0;
+}
