e577f5e3b5385db1ae74ee04fd310f14d1f35ba0
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-16 19:59:22 +0500

    Updated the display function to accept the array size as a parameter. The loop in the display function now runs according to the length of the array, allowing for flexibility in array size.

--- 5. Functions in C++/4. Functions with Arrays in C++/1_Example.cpp
+++ 5. Functions in C++/4. Functions with Arrays in C++/1_Example.cpp
@@ -4,16 +4,17 @@
 
 using namespace std;
 
-void display (int marks[5]) {
+void display (int marks[], int size) {
 	cout << "The Elemets of the Array are :- \n";
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < size; i++)
 		cout << marks[i] << endl;
 }
 
 int main() {
-    
-	int marks[5] = { 3,5,7,9,5 };
-	display(marks);
+
+	int marks[] = { 3,5,7,9,5 };
+    int size = sizeof(marks)/sizeof(marks[0]);
+	display(marks,size);
 	
 	_getch();
 	return 0;
