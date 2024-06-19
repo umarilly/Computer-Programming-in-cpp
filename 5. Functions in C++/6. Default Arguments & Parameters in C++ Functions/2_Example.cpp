32c246e9ff25a105ce211eff22dfefd3ee59a18f
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-19 20:32:13 +0500

    Added multiple calls to the sum function to demonstrate the behavior of default parameters in C++. The sum function is called with two arguments sum(a, b), one argument sum(a), one argument sum(b), and no arguments sum(). This showcases how the default parameter values are utilized when fewer than two arguments are provided.

---
+++ 5. Functions in C++/6. Default Arguments & Parameters in C++ Functions/2_Example.cpp
@@ -0,0 +1,24 @@
+
+// Program to understand the behavior of default parameters - Just the sum of 2 numbers
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+void sum (int x = 15, int y = 16) {
+	x + y;
+	cout << "Sum is : " << x + y << endl;
+}
+
+int main() {
+
+	int a = 10, b = 5;
+
+	sum(a, b); 
+    sum(a);
+    sum(b);
+    sum();  
+
+	_getch();
+	return 0;
+}
