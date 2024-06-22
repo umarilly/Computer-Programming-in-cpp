577febe1e39fc9d3ccc2a0124630eba2927e069f
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-22 21:46:26 +0500

    Added comments to explain the use of the reference operator and the output of the code. The comments clarify that printing the variable a will output its value, '100', and printing &a will output the memory address of a, which is its actual location in RAM. This enhancement aims to make the code more understandable for readers by providing context on how the reference operator works and what the printed outputs represent.

--- 6. Pointers in C++/1. Basics of Pointers in C++/1_Example.cpp
+++ 6. Pointers in C++/1. Basics of Pointers in C++/1_Example.cpp
@@ -1,16 +1,18 @@
 
+// Example to explain how the reference operator works
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
 int main() {
-    
-	int a = 100;
-	cout << a;
-	cout << endl;
-	cout << &a; 
 
-	_getch();
-	return 0;
+    int a = 100;
+    cout << a;     // This will print out "100"
+    cout << endl;
+    cout << &a;    // This will print the memory address of the variable "a"
+    // The memory address is the actual location in RAM where the variable "a" is stored.
+    _getch();
+    return 0;
 }
