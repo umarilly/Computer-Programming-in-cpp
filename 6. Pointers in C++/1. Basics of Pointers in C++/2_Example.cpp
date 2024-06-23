401ca86bf3683c8f104aef5d3c23e4ae2574667e
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-23 23:26:07 +0500

    Added comments to the program to explain how the reference operator works. The program demonstrates that each variable has its own memory location. The comments clarify that the values of the variables a and b are printed first, followed by their respective memory addresses. This illustrates that even though both variables hold the same value, they occupy distinct memory locations.

--- 6. Pointers in C++/1. Basics of Pointers in C++/2_Example.cpp
+++ 6. Pointers in C++/1. Basics of Pointers in C++/2_Example.cpp
@@ -1,4 +1,7 @@
 
+// Example to explain how the reference operator works
+// This example will further explain that each variable has its own mamory loaction  
+
 #include<iostream>
 #include<conio.h>
 
@@ -8,10 +11,12 @@ int main() {
 
 	int a = 10;
     int b = 10;
-
+    // This will simply print out the values i.e., "10"
 	cout << "Value stored in a is : " << a << endl;
 	cout << "Value stored in b is : " << b << endl;
+
 	cout << endl;
+    // This will print the exect memory address of these variables
 	cout << "The address of a is : " << &a << endl;
 	cout << "The address of b is : " << &b << endl;
 
