0fd0b99fe26887ff729d3b61ca4b92be0f3e475d
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-26 09:44:51 +0500

    Implemented an example to explain the basic use of pointers in C++. This program demonstrates how to declare a pointer, assign it the address of an integer variable, and use the pointer to access and print both the value and address of the variable. Added comments to clarify each step in the code.

--- 6. Pointers in C++/1. Basics of Pointers in C++/5_Example.cpp
+++ 6. Pointers in C++/1. Basics of Pointers in C++/5_Example.cpp
@@ -1,16 +1,21 @@
 
+// Example to explain the basic use of pointers
+
 #include <iostream>
 #include <conio.h>
+
 using namespace std;
+
 int main()
 {
-    int a;
+    int a = 7;
     int *aPtr;
-    a = 7;
     aPtr = &a;
 
-    cout << "The address of a is " << &a << "\nThe value of aPtr is " << aPtr;
-    cout << "\n\nThe value of a is " << a << "\nThe value of *aPtr is " << *aPtr;
+    cout << "The value of a is " << a << endl; // Prints the value of "a" which is 7
+    cout << "The value of *aPtr is " << *aPtr << endl; // Prints the value "7", dereferencing the pointer aPtr
+    cout << "The address of a is " << &a << endl; // Prints the address of a
+    cout << "The value of aPtr is " << aPtr << endl; // Prints the address stored in aPtr - which is the address of a variable
 
     _getch();
     return 0;
