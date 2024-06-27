d8382d773027292895a8967d7cf4b64fc99cb3de
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-27 09:30:31 +0500

    Implemented an example to demonstrate the concept of reference and dereference operators in pointers. Added code to show how * and & are inverses of each other, with comments explaining each output. The program prints the memory address of a variable, the memory address stored in a pointer, the value of the variable, and the value pointed to by the pointer. Additionally, it verifies that & and * are inverses by printing the address of the value pointed to by the pointer and the value stored in the pointer.

--- 6. Pointers in C++/1. Basics of Pointers in C++/6_Example.cpp
+++ 6. Pointers in C++/1. Basics of Pointers in C++/6_Example.cpp
@@ -1,23 +1,25 @@
 
+// Example to show the concept of reference and dereference operator in pointers
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
 int main() {
-    
-	int a; 
+
+	int a = 10; 
 	int* b; 
-	a = 7;
 	b = &a; 
-	
-	cout << "The address of a is " << &a << "\nThe value of b is " << b;
-	cout << "\n\nThe value of a is " << a  << "\nThe value of *b is " << *b;
 
-	cout << "\n\n";
-	cout << "Showing that * and & are inverses of each other" << endl;
-
-	cout << "&*b = " << &*b << "\n *&b = " << *&b << endl;
+	cout << "----- Showing that * and & are inverses of each other -----\n\n";
+	cout << "The address of a is : " << &a << endl; // Prints the memory address of the variable "a"
+    cout << "The value of b is : " << b << endl; // Prints the memory address stored in pointer "b", which is the address of "a"
+	cout << "The value of a is : " << a  << endl; // Prints the value of "a", which is 10
+    cout << "The value of *b is : " << *b << endl; // Prints the value pointed to by "b", which is the value of "a" (10)
+    cout << endl;
+	cout << "&*b : " << &*b << endl; // Prints the address of the value pointed to by "b", which is the address of "a"
+    cout << "*&b : " << *&b << endl; // Prints the value stored in "b", which is the address of "a"
 
 	_getch();
 	return 0;
