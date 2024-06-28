68e887d3a6e1ad74a942712e7f8ae1652b37418c
Muhammad Umar <Muhammadumar695@gmail.com>
2024-06-28 09:43:47 +0500

    Implemented a function to demonstrate the use of pointers with reference and dereference operators. The callByPointer function modifies the value of a variable by dereferencing a pointer and cubing the original number. Comments were added to explain the use of pointers, the reference operator (&), and the dereference operator (*). cout statements in both the main function and callByPointer print the value and memory address of the variable before and after the function call, showing that the memory address of the variable remains unchanged.

--- 6. Pointers in C++/2. Call by Address or Pointer/1_Example.cpp
+++ 6. Pointers in C++/2. Call by Address or Pointer/1_Example.cpp
@@ -1,20 +1,22 @@
-
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-void callbypointer(int* number_1) {
-	*number_1 = *number_1 * *number_1 * *number_1;
+void callByPointer(int* number) {
+	*number = *number * *number * *number; // Dereferencing the pointer to modify the value of the variable it points to
+    cout << "Inside callByPointer function, the cubed value is : " << *number << endl; // Printing the cubed value of the original number
+    cout << "Inside callByPointer function, the memory address stored in the pointer is : " << number << endl; // Printing the memory address stored in the pointer
 }
 
 int main() {
+    int number = 5;
+	cout << "The value stored in number variable is : " << number << endl; // Printing the initial value of the variable 'number'
+    cout << "The memory address of number variable is : " << &number << endl; // Printing the memory address of the variable 'number'
+	callByPointer(&number);
+    cout << "The new value stored in number variable is : " << number << endl; // Printing the modified value of the variable 'number' after the function call
+    cout << "The memory address of number variable is still : " << &number << endl; // Printing the memory address of the variable 'number' to show it remains the same
 
-	int number = 5;
-	cout << "The original value is : " << number << endl;
-	callbypointer(&number);
-	cout << "The new value is : " << number << endl;
-
-	_getch();
-	return 0;
+    _getch();
+    return 0;
 }
