7502668431853609a7274ede4bb0d6553821abf2
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-01 02:08:11 +0500

    Added a program demonstrating the concept of assignment in pointers. The program initializes three integer variables and three pointers pointing to these variables. It prints the initial values and memory addresses of the variables, as well as the values and memory addresses stored in the pointers. The program then reassigns one pointer to point to the same address as another pointer and updates the value of one variable through the pointer. Finally, it prints the updated memory addresses and values, demonstrating the effect of pointer assignment and dereferencing on the variables.

--- 6. Pointers in C++/3. Assignment in Pointers/1_Example.cpp
+++ 6. Pointers in C++/3. Assignment in Pointers/1_Example.cpp
@@ -7,40 +7,46 @@
 using namespace std;
 
 int main() {
-	
-	int a = 100, b = 88, c = 8;
 
-	int* p1 = &a, *p2 = &b, *p3 = &c;
+    int a = 100, b = 88, c = 8; // Initializing three integer variables
 
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
-	cout << "c = " << c << "\n\n";
-	
-	cout << "&a = " << &a << endl;
-	cout << "&b = " << &b << endl;
-	cout << "&c = " << &c << "\n\n";
+    int* p1 = &a, *p2 = &b, *p3 = &c; // Initializing three pointers pointing to the variables
 
-	cout << "*p1 = " << *p1 << endl;
-	cout << "*p2 = " << *p2 << endl;
-	cout << "*p3 = " << *p3 << "\n\n";
+    // Printing the initial values of a, b, and c
+    cout << "a = " << a << endl; 
+    cout << "b = " << b << endl; 
+    cout << "c = " << c << "\n\n"; 
 
-	cout << "p1 = " << p1 << endl;
-	cout << "p2 = " << p2 << endl;
-	cout << "p3 = " << p3 << "\n\n";
+    // Printing the memory addresses of a, b, and c
+    cout << "&a = " << &a << endl; 
+    cout << "&b = " << &b << endl; 
+    cout << "&c = " << &c << "\n\n"; 
 
-	p2 = p1;
-	b = *p3; 
-	*p2 = *p3;
-	
-	cout << "p2 = " << p2 << endl;
-	cout << "p1 = " << p1 << endl;
-	cout << "*p3 = " << *p3 << endl;
-	cout << "b = " << b << endl;
-	cout << "*p3 = " << *p3 << endl;
-	cout << "*p2 = " << *p2 << "\n\n";
+    // Printing the values pointed to by p1, p2, and p3
+    cout << "*p1 = " << *p1 << endl; 
+    cout << "*p2 = " << *p2 << endl; 
+    cout << "*p3 = " << *p3 << "\n\n"; 
 
-	cout << a << " " << b << " " << c;
-	
-	_getch();
-	return 0;
+    // Printing the memory addresses stored in p1, p2, and p3
+    cout << "p1 = " << p1 << endl; 
+    cout << "p2 = " << p2 << endl; 
+    cout << "p3 = " << p3 << "\n\n"; 
+
+    p2 = p1;      // Assigning p2 to point to the same address as p1
+    b = *p3;     // Assigning the value pointed to by p3 to b
+    *p2 = *p3;  // Assigning the value pointed to by p3 to the location pointed to by p2
+
+    // Printing the updated memory addresses and values
+    cout << "p2 = " << p2 << endl; 
+    cout << "p1 = " << p1 << endl; 
+    cout << "*p3 = " << *p3 << endl; 
+    cout << "b = " << b << endl; 
+    cout << "*p3 = " << *p3 << endl; 
+    cout << "*p2 = " << *p2 << "\n\n"; 
+
+    // Printing the final values of a, b, and c
+    cout << a << " " << b << " " << c; 
+
+    _getch(); // Waiting for a key press
+    return 0;
 }
