a7239873a56163506a602a909821b66c940b8e86
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-02 09:25:20 +0500

    Added program to demonstrate the concept of pointers in C++. This program initializes an integer variable v1 with the value 10 and a pointer p1 that points to the address of v1. The program then updates the value of v1 to 42 through dereferencing p1. It prints the value of v1 and the value pointed to by p1 to confirm the update. Another pointer p2 is declared and assigned to the address stored in p1. The value pointed to by p2 is printed to verify that it correctly points to the updated value of v1. This program provides a clear example of pointer initialization, dereferencing, and assignment.

--- 6. Pointers in C++/3. Assignment in Pointers/2_Example.cpp
+++ 6. Pointers in C++/3. Assignment in Pointers/2_Example.cpp
@@ -1,4 +1,6 @@
 
+// Example to explain the working of pointers in C++
+
 #include<iostream>
 #include<conio.h>
 
@@ -6,19 +8,19 @@ using namespace std;
 
 int main() {
 	
-	int v1 = 0; 
-	int *p2;
-	int *p1 = &v1; 
+	int v1 = 10;  // Initializing an integer variable v1 with the value 10
+	int *p1 = &v1;  // Declaring a pointer p1 and initializing it with the address of v1
 	
-	*p1 = 42;
+	*p1 = 42;  // Dereferencing p1 to update the value of v1 to 42
 
-	cout << v1 << endl;
-	cout << *p1 << endl;
+	cout << v1 << endl;  // Printing the value of v1, which should now be 42
+	cout << *p1 << endl;  // Printing the value pointed to by p1, which should also be 42
 
-	p2 = p1;
+    int *p2;  // Declaring another pointer p2
+	p2 = p1;  // Assigning p2 to point to the same address as p1
 
-	cout << *p2 << endl;
+	cout << *p2 << endl;  // Printing the value pointed to by p2, which should be 42
 
-	_getch();
-	return 0;
+	_getch();  
+	return 0; 
 }
