c42d8f30d2dbe470f7b3a2233f6b6265857e86f0
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-03 09:22:07 +0500

    Added a program to demonstrate the concept of pointer manipulation and arithmetic in C++. This program initializes an integer variable v1 with the value 0 and declares two pointers p1 and p2. The pointer p1 is assigned the address of v1, and the value of v1 is updated to 42 through dereferencing p1. The program prints the value of v1 and the value pointed to by p1, using the post-increment operator on p1 to move the pointer to the next memory address. The pointer p2 is then assigned the new address stored in p1. Finally, the value pointed to by p2 is printed. This program provides a clear example of pointer initialization, dereferencing, pointer arithmetic, and assignment.

---
+++ 6. Pointers in C++/3. Assignment in Pointers/3_Example.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+	
+	int v1 = 0; 
+	int* p2;
+	int* p1 = &v1;
+	* p1 = 42;
+
+	cout << v1 << endl;
+	cout << *p1++ << endl;
+	p2 = p1;
+	cout << *p2;
+
+	_getch();
+	return 0;
+}
