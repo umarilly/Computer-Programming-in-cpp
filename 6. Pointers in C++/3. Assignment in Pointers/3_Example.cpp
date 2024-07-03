705dd00ce2e66d70f96ac26991c12170d419421d
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-03 09:40:28 +0500

    Implemented a program that demonstrates how pointers can be used to modify the value of a variable and how one pointer can be assigned to another, making them both point to the same address. The program initializes an integer variable v1 and a pointer p1 that holds the address of v1. It then modifies the value of v1 through p1. The program prints the value and memory address of v1, the value pointed to by p1, and the address stored in p1. Another pointer p2 is then assigned to p1, and the program demonstrates that p2 can also be used to access and display the modified value of v1. This illustrates the concept of pointer assignment and dereferencing.

--- 6. Pointers in C++/3. Assignment in Pointers/3_Example.cpp
+++ 6. Pointers in C++/3. Assignment in Pointers/3_Example.cpp
@@ -1,4 +1,6 @@
 
+// The program demonstrates how pointers can be used to modify the value of a variable and how one pointer can be assigned to another, making them both point to the same address.
+
 #include<iostream>
 #include<conio.h>
 
@@ -6,16 +8,20 @@ using namespace std;
 
 int main() {
 	
-	int v1 = 0; 
-	int* p2;
-	int* p1 = &v1;
-	* p1 = 42;
+	int v1 = 0;  // Initializing integer variable v1 with value 0
+	int* p1 = &v1;  // Initializing pointer p1 with the address of v1
+	*p1 = 42;  // Dereferencing p1 to set the value of v1 to 42
 
-	cout << v1 << endl;
-	cout << *p1++ << endl;
-	p2 = p1;
-	cout << *p2;
+    int* p2;    // Declaring an integer pointer p2
 
-	_getch();
-	return 0;
+	cout << v1 << endl;  // Printing the value of v1, expected output: 42
+    cout << &v1 << endl; // Printing the memory address of v1
+    cout << *p1 << endl; // Dereferencing pointer p1 to display the value of 42
+    cout << p1 << endl;  // Printing the address stored in p1 (address of v1)
+
+	p2 = p1;  // Assigning p1 to p2, so p2 also points to the address of v1
+	cout << *p2;  // Dereferencing p2 to display the value at the address of v1, expected output: 42
+
+	_getch();  
+	return 0;  
 }
