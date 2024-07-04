f4c9ec06d60adfe5fba59c8b6758e76b241c1180
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-04 09:23:16 +0500

    Implemented a program that demonstrates how pointers can be used to modify the value of a variable and how one pointer can be assigned to another. It initializes an integer variable v1 and a pointer p1 that holds the address of v1. The value of v1 is modified through p1, and the program prints the value of v1, its memory address, the value pointed to by p1, and the address stored in p1. Another pointer p2 is assigned the address of v1, and the value pointed to by p2 is set to the value pointed to by p1. The program then prints the value pointed to by p2, showing that both pointers can access and modify the same variable.

--- 6. Pointers in C++/3. Assignment in Pointers/4_Example.cpp
+++ 6. Pointers in C++/3. Assignment in Pointers/4_Example.cpp
@@ -1,4 +1,6 @@
 
+// The program demonstrates how pointers can be used to modify the value of a variable and how one pointer can be assigned to another.
+
 #include<iostream>
 #include<conio.h>
 
@@ -8,15 +10,18 @@ int main() {
 	
 	int v1 = 0; 
 	int* p1 = &v1;
-	* p1 = 42;
+	* p1 = 10;
 
-	cout << v1 << endl;
-	cout << *p1 << endl;
+	cout << v1 << endl;  // Printing the value of v1, expected output: 10
+    cout << &v1 << endl; // Printing the memory address of v1
+    cout << *p1 << endl; // Dereferencing pointer p1 to display the value of 10
+    cout << p1 << endl;  // Printing the address stored in p1 (address of v1)
 
 	int* p2;
 	p2 = &v1;
 	*p2 = *p1;
-	cout << *p2 << endl;
+
+	cout << *p2 << endl; // This will also print 10, as it also holds same address as p1
 
 	_getch();
 	return 0;
