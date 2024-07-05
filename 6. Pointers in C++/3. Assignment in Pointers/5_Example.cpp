f5f5d0aef5c688d9f899d53e4d3e5df4e84065e4
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-05 09:12:36 +0500

    Implemented a program that demonstrates pointer usage and assignment. The program initializes an integer variable v1 with the value 0 and then assigns its address to the pointer p1. By dereferencing p1, the value of v1 is set to 42. An additional pointer p2 is declared and assigned the address of v1. The program then prints the value of v1, the memory address of v1, the value pointed to by p1, and the address stored in p1. It further prints the memory address of v1 and the address stored in p2, followed by dereferencing p2 to display the value of 42. This exercise effectively illustrates how pointers can be used to modify the value of a variable and how one pointer can be assigned to another to point to the same memory address.

--- 6. Pointers in C++/3. Assignment in Pointers/5_Example.cpp
+++ 6. Pointers in C++/3. Assignment in Pointers/5_Example.cpp
@@ -1,31 +1,33 @@
 
-#include<iostream>
-#include<conio.h>
+// Program to demonstrate pointer usage and assignment
+
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
-    
-	int v1 = 0; 
-	int* p1 = &v1;
-	* p1 = 42;
 
-    int* p2;
+    int v1 = 0;    // Initializing integer variable v1 with value 0
+    int *p1 = &v1; // Initializing pointer p1 with the address of v1
+    *p1 = 42;      // Dereferencing p1 to set the value of v1 to 42
 
-	cout << &v1 << endl;
-	cout << v1 << endl;
-	cout << *p1 << endl;
-    cout << p1 << endl;
+    int *p2; // Declaring an integer pointer p2
 
-	p2 = &v1;
-	
-	cout << &v1 << endl;
-	cout << p2 << endl;
-	
-	*p2 = *p1;
+    cout << v1 << endl;  // Printing the value of v1, expected output: 42
+    cout << &v1 << endl; // Printing the memory address of v1
+    cout << *p1 << endl; // Dereferencing pointer p1 to display the value of 42
+    cout << p1 << endl;  // Printing the address stored in p1 (address of v1)
 
-	cout << *p2 << endl;
+    p2 = &v1; // Assigning the address of v1 to pointer p2
 
-	_getch();
-	return 0;
+    cout << &v1 << endl; // Printing the memory address of v1 again
+    cout << p2 << endl;  // Printing the address stored in p2 (address of v1)
+
+    *p2 = *p1; // Assigning the value pointed to by p1 to the location pointed to by p2
+
+    cout << *p2 << endl; // Dereferencing p2 to display the value of 42
+
+    _getch();
+    return 0;
 }
