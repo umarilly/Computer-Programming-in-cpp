d8511db07da63e4014c748bb875452f50b246495
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-06 23:02:33 +0500

    Implemented a program to demonstrate the usage of pointers with arrays. This program initializes an array of integers and a pointer to the array. It then prints each element's value and memory address using both array indexing and pointer arithmetic. The program also highlights how to use pointer dereferencing to access and manipulate array elements. This serves as a basic example to understand the interaction between arrays and pointers in C++.

--- 6. Pointers in C++/4. Pointers with Arrays in C++/1_Example.cpp
+++ 6. Pointers in C++/4. Pointers with Arrays in C++/1_Example.cpp
@@ -1,36 +1,43 @@
 
-#include<iostream>
-#include<conio.h>
+// Example to explain how pointers work with arrays, and How pointer arithmetic works.
+
+#include <iostream> 
+#include <conio.h>  
 
 using namespace std;
 
-int main() {
-	int numbers[5] = {10,20,30,40,50};
-	int* p;
-	p = numbers;
+int main()
+{
+    int numbers[] = {10, 20, 30, 40, 50}; // Initialize an array of integers with 5 elements
+    int size = sizeof(numbers) / sizeof(numbers[0]); // Calculate the number of elements in the array
+    int *p; // Declare an integer pointer p
+    p = numbers; // Initialize pointer p to point to the first element of the array
 
-	for (int n = 0; n < 5; n++)
-		cout << numbers[n] << " ";
+    // Print each element of the array from index 0 to n
+    for (int n = 0; n < size; n++) {
+        cout << numbers[n] << " "; // Output the value of each element in the array
+    }
+    cout << endl;
 
-	cout << endl;
+    // Print the memory address of each element of the array from 0 to n
+    for (int n = 0; n < size; n++) {
+        cout << &numbers[n] << " "; // Output the memory address of each element in the array
+    }
+    cout << endl;
 
-	for (int n = 0; n < 5; n++)
-		cout << p[n] << " ";
+    // Print each element of the array using pointer dereferencing
+    for (int n = 0; n < size; n++) {
+        cout << p[n] << " "; // Output the value of each element in the array using the pointer
+        // We can also use *(p+n) instead of p[n], as they both give same output.
+    }
+    cout << endl;
 
-	cout << endl;
-	
-	cout << numbers[0];
-	cout << endl;
+    // Print the memory address of each element using pointer arithmetic
+    for (int n = 0; n < size; n++) {
+        cout << (p + n) << " "; // Output the memory address of each element using pointer arithmetic
+    }
+    cout << endl;
 
-	cout << *p;
-	cout << endl;
-
-	cout << numbers;
-	cout << endl;
-
-	cout << p;
-	cout << endl;
-
-	_getch();
-	return 0;
+    _getch(); 
+    return 0;
 }
