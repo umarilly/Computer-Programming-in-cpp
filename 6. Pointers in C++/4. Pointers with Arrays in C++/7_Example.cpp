1ab4548f7e0b1b88b7c9a7c9a94ea2bd27e457f0
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-13 12:41:41 +0500

    Added a program to demonstrate the use of pointers and pointer arithmetic to manipulate elements in an array. The program initializes an array of integers and uses a pointer to assign values to each element through pointer arithmetic. It outputs the values of the array elements both individually through dereferencing and collectively using array indexing. This example showcases basic pointer operations and array handling in C++.

---
+++ 6. Pointers in C++/4. Pointers with Arrays in C++/7_Example.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    int numbers[5];
+    int *p;
+    p = numbers;
+
+    *p = 10;
+    cout << *p;
+    cout << endl;
+
+    p = numbers + 1;
+    *p = 20;
+    cout << *p;
+    cout << endl;
+
+    p = numbers + 2;
+    *p = 30;
+    cout << *p;
+    cout << endl;
+
+    p = numbers + 3;
+    *p = 40;
+    cout << *p;
+    cout << endl;
+
+    p = numbers + 4;
+    *p = 50;
+    cout << *p;
+    cout << endl;
+
+    for (int n = 0; n < 5; n++)
+    {
+        cout << numbers[n] << " ";
+    }
+
+    _getch();
+    return 0;
+}
