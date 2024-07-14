dd3f5f98358d2e7cd6c506b060d421ffe7f5c93c
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-14 15:54:42 +0500

    Added a C++ program to demonstrate the use of pointers for accessing and printing elements of an array. This program initializes an array of integers with five elements and declares an integer pointer. The pointer is set to point to the first element of the array, and pointer arithmetic is used to traverse the array. The program outputs each element of the array by incrementing the pointer and dereferencing it. Additionally, a loop is included to print each element of the array using array indexing, showcasing both methods of accessing array elements. This example provides a clear demonstration of how pointers and pointer arithmetic can be used in conjunction with arrays in C++.

--- 6. Pointers in C++/4. Pointers with Arrays in C++/8_Example.cpp
+++ 6. Pointers in C++/4. Pointers with Arrays in C++/8_Example.cpp
@@ -1,35 +1,36 @@
+
+// Program to demonstrate the use of pointers for accessing and printing elements of an Array 
+
 #include <iostream>
 #include <conio.h>
+
 using namespace std;
 
 int main() {
 
-    int numbers[5] = {10, 20, 30, 40, 50};
-    int *p;
-    p = numbers;
+    int numbers[5] = {10, 20, 30, 40, 50}; // Initialize an array of integers with 5 elements
+    int *p;                               // Declare an integer pointer
+    p = numbers;                          // Set the pointer to point to the first element of the array
 
-    cout << *p;
-    cout << endl;
+    cout << *p << "\n";                   // Output the value of the first element
 
-    p = numbers + 1;
-    cout << *p;
-    cout << endl;
+    p = numbers + 1;                      // Set the pointer to point to the second element of the array
+    cout << *p << "\n";                   // Output the value of the second element
 
-    p = numbers + 2;
-    cout << *p;
-    cout << endl;
+    p = numbers + 2;                      // Set the pointer to point to the third element of the array
+    cout << *p << "\n";                   // Output the value of the third element
 
-    p = numbers + 3;
-    cout << *p;
-    cout << endl;
+    p = numbers + 3;                      // Set the pointer to point to the fourth element of the array
+    cout << *p << "\n";                   // Output the value of the fourth element
 
-    p = numbers + 4;
-    cout << *p;
-    cout << endl;
+    p = numbers + 4;                      // Set the pointer to point to the fifth element of the array
+    cout << *p << "\n";                   // Output the value of the fifth element
 
-    for (int n = 0; n < 5; n++)
-        cout << numbers[n] << " ";
+    // Loop to print each element of the array using array indexing
+    for (int n = 0; n < 5; n++) {
+        cout << numbers[n] << " ";        // Output the value of each element in the array
+    }
 
-    _getch();
-    return 0;
+    _getch();                       
+    return 0;             
 }
