30aa4462671d4fd3e5fdba517784008883c4c3ab
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-24 10:35:28 +0500

    Added a program that initializes two character arrays, 'Hello' and 'World', and uses pointer arithmetic to display each array. Implemented string concatenation using strcpy and strcat functions from the <cstring> library to combine the two arrays into a new array, concatenatedArray. The program outputs each individual array and the concatenated result. This demonstrates the use of pointers in array traversal and basic string manipulation in C++.

--- 6. Pointers in C++/6. Pointers with Character Arrays - ( C - String )/4_Example.cpp
+++ 6. Pointers in C++/6. Pointers with Character Arrays - ( C - String )/4_Example.cpp
@@ -1,22 +1,41 @@
 
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
+#include <cstring> 
 
 using namespace std;
 
-int main() {
+int main()
+{
 
-	char abc[] = "Hello World";
-	char* p;
-	p = abc;
+    char firstArray[] = "Hello";
+    int sizeOfFirstArray = sizeof(firstArray) / sizeof(char);
+    char *ptrFirst;
+    ptrFirst = firstArray;
 
-	int size = sizeof(abc) / sizeof(char);
+    cout << "The First Character Array is : " << endl;
+    for (int i = 0; i < sizeOfFirstArray; i++) {
+        cout << *(ptrFirst++);
+    }
+    cout <<  endl;
 
-	cout << abc << endl;
+    char secondArray[] = "World";
+    int sizeOfSecondArray = sizeof(secondArray) / sizeof(char);
+    char *ptrSecond;
+    ptrSecond = secondArray;
 
-	for (int i=0 ; i<size;i++)
-	cout << *(p++) ;
+    cout << "The Second Character Array is : " << endl;
+    for (int i = 0; i < sizeOfSecondArray; i++) {
+        cout << *(ptrSecond++);
+    }
+    cout <<  endl;
 
-	_getch();
-	return 0;
+    char concatenatedArray[sizeof(firstArray) + sizeof(secondArray) - 1];
+    strcpy(concatenatedArray, firstArray); 
+    strcat(concatenatedArray, secondArray);
+
+    cout << "The Concatenated Array is : " << concatenatedArray << endl; 
+
+    _getch();
+    return 0;
 }
