de54332653f4f8ac161d4ec042c86b1db5c3174d
Muhammad Umar <Muhammadumar695@gmail.com>
2024-07-29 09:44:27 +0500

    Implemented a program to demonstrate basic pointer operations in C++. The program initializes an integer variable a and uses a pointer b to store its address, illustrating how to access the value and address using pointers. It further introduces a double pointer c to point to b, showing how to navigate through multiple levels of indirection. The program outputs the values and addresses of the variables involved, providing a clear understanding of pointer mechanics and memory access in C++. This serves as a foundational example for understanding pointers and double pointers.

--- 6. Pointers in C++/7. Pounter to Pointer/1_Example.cpp
+++ 6. Pointers in C++/7. Pounter to Pointer/1_Example.cpp
@@ -1,4 +1,6 @@
 
+// Program to explain the concept of Pointer to Pointer 
+
 #include <iostream>
 #include <conio.h>
 
@@ -6,24 +8,24 @@ using namespace std;
 
 int main() {
 
-    int a = 10;
-    int *b;
-    b = &a;
-    int **c;
+    int a = 10; // Initialize an integer variable a with value 10
+    int *b;     // Declare a pointer to an integer
+    b = &a;     // Store the address of a in pointer b
 
-    cout << a << endl;
-    cout << *b << endl;
-    cout << &a << endl;
-    cout << b << endl;
+    cout << "The Value of a is : " << a << endl;         // Output the value of a
+    cout << "The Value pointed to by b is : " << *b << endl; // Output the value pointed to by b (value of a)
+    cout << "The Address of a is : " << &a << endl;      // Output the address of a
+    cout << "The Value of b (Address of a) is : " << b << endl; // Output the value of b (address of a)
     cout << endl;
 
-    c = &b;
+    int **c;    // Declare a pointer to a pointer to an integer
+    c = &b;     // Store the address of b in pointer c
 
-    cout << b << endl;
-    cout << *c << endl;
-    cout << **c << endl;
-    cout << &b << endl;
-    cout << c << endl;
+    cout << "The Value of b (Address of a) is : " << b << endl; // Output the value of b (address of a)
+    cout << "The Value pointed to by c (Address of a) is : " << *c << endl; // Output the value pointed to by c (address of a)
+    cout << "The Value pointed to by *c (Value of a) is : " << **c << endl; // Output the value pointed to by *c (value of a)
+    cout << "The Address of b is : " << &b << endl;         // Output the address of b
+    cout << "The Value of c (Address of b) is : " << c << endl; // Output the value of c (address of b)
 
     _getch();
     return 0;
