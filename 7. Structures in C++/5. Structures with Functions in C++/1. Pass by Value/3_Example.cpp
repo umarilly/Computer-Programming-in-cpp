9de1a081e98d678233f4fd538c6879153ee70275
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-01 23:38:49 +0500

    Implemented a program to demonstrate passing a struct by value to a function, emphasizing the creation of a copy of the Rectangle object within the display function, and illustrating how this approach affects data modification and code organization. Improved code readability with comments and proper naming conventions.

--- 7. Structures in C++/5. Structures with Functions in C++/1. Pass by Value/3_Example.cpp
+++ 7. Structures in C++/5. Structures with Functions in C++/1. Pass by Value/3_Example.cpp
@@ -1,31 +1,37 @@
 
+// Program to demonstrate passing a struct by value to a function
+
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
 
 using namespace std;
 
+// Define a struct to represent a Rectangle with length and width properties
 struct Rectangle {
-	int length;
-	int width;
+	int length; // Length of the rectangle
+	int width;  // Width of the rectangle
 };
 
-void display(Rectangle p) {
-	cout << "\n" << endl;
-	cout << " ===> The length of Rectangle Is : " << p.length << endl;
-	cout << " ===> The width of Rectangle Is : " << p.width << endl;
+// Function to display the properties of a Rectangle
+void display(Rectangle R) {
+	cout << "\n\n ===> The length of rectangle is : " << R.length << endl; 
+	cout << " ===> The width of rectangle is : " << R.width << endl; 
+    cout << "\n ===> The Area of rectangle is : " << R.length * R.width << endl; 
 }
 
 int main()
 {
-	Rectangle p;
-	cout << endl;
-	cout << " Enter length : ";
-	cin >> p.length;
-	cout << " Enter wdith : ";
-	cin >> p.width;
-    
-	display(p);
+	Rectangle R;
+
+	// Prompt user for input and store in Rectangle object
+	cout << "\n ===> Enter the length of rectangle : ";
+	cin >> R.length;
+	cout << " ===> Enter width of rectangle : ";
+	cin >> R.width;
+
+	// Call display function to show Rectangle properties
+	display(R);
 
 	_getch();
 	return 0;
