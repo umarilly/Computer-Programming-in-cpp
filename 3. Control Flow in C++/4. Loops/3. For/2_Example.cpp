0a484afddcd5c7c62918f102b9590cc60dad06aa
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-05 19:14:20 +0500

    Added a C++ program to demonstrate the use of a for loop with two variables. The program initializes two variables, 'a' and 'b', where 'a' increments from 1 to 10 and 'b' decrements from 10 to 1. The loop runs 10 times, printing the values of both 'a' and 'b' in each iteration. This serves as an example of how a for loop can manage multiple variables simultaneously and control their progression in opposite directions.

--- 3. Control Flow in C++/4. Loops/3. For/2_Example.cpp
+++ 3. Control Flow in C++/4. Loops/3. For/2_Example.cpp
@@ -1,14 +1,21 @@
+
+/*
+    Program in C++ to demonstrate the use of for loop.
+*/
+
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 int main()
 {
-    int a, b;
+    int a, b; // variable declaration
 
     for (a = 1, b = 10; a <= 10, b >= 1; a++, b--) {
-        cout << "a=" << a << "\tb=" << b << endl;
+        // This block of code will be executed 10 times
+        cout << "a : " << a << "\tb : " << b << endl;
     }
 
-
+    _getch();
     return 0;
 }
