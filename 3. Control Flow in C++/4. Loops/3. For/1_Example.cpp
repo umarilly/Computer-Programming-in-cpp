dc083038c949530b67974ff78e91d98d6351cfd5
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-05 19:10:58 +0500

    Implemented a C++ program to demonstrate a for loop. The program initializes a variable 'a' and uses a for loop to iterate from 1 to 10, printing the value of 'a' during each iteration. This is a basic example of looping in C++ to repeat a block of code a specified number of times.

--- 3. Control Flow in C++/4. Loops/3. For/1_Example.cpp
+++ 3. Control Flow in C++/4. Loops/3. For/1_Example.cpp
@@ -1,13 +1,24 @@
+
+/*
+    Program in C++ to demonstrate the use of for loop.
+*/
+
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
+
 int main() {
-    int a;
+    
+    int a; // variable declaration
+
+    // for loop to print numbers from 1 to 10
 
     for (a = 1; a <= 10; a++) {
-            cout << "a=" << a << endl;
+        // This block of code will be executed 10 times
+            cout << "a : " << a << endl;
         }
 
-    
+    _getch();
     return 0;
 }
