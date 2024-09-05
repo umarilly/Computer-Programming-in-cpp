bbededbe7a615deaecdba9254239c2083b3d5ba9
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-05 19:33:30 +0500

    Implemented a C++ program that demonstrates the use of a for loop to calculate the sum of even numbers from 2 to 100. The program initializes a variable 'a' to start at 2 and iterates by adding 2 each time, ensuring only even numbers are summed. The sum is stored in the variable 'b', which is updated with each iteration of the loop. After the loop completes, the total sum of even numbers is displayed, providing an efficient way to compute the result using a loop.

--- 3. Control Flow in C++/4. Loops/3. For/3_Example.cpp
+++ 3. Control Flow in C++/4. Loops/3. For/3_Example.cpp
@@ -1,4 +1,8 @@
 
+/*
+    Program in C++ to demonstrate the use of for loop.
+*/
+
 #include <iostream>
 #include <conio.h>
 
@@ -6,12 +10,15 @@ using namespace std;
 
 int main()
 {
-    int a, b = 0;
+    int a;
+    int b = 0;
     
     for (a = 2; a <= 100; a = a + 2) {
+        // This block of code will be executed 50 times
         b = b + a;
     }
 
+    // This line of code will be executed only once and always executed
     cout << "The sum of even numbers from zero to hundrad is : " << b << endl;
 
     _getch();
