c2c3d81f4c61e24e2d6af4a6ee5a84d9c33e9376
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-06 20:28:19 +0500

    Added a C++ program to demonstrate the use of the continue statement in an infinite while loop. The program prompts the user to enter a number and uses the continue statement to skip the rest of the loop when a non-negative number is entered. The loop terminates with a break when the user inputs a negative number. This illustrates how to control loop behavior with continue and break statements. Clear comments and prompts are included to enhance code understanding.

--- 3. Control Flow in C++/5. Break and Continue Statement/2_Example.cpp
+++ 3. Control Flow in C++/5. Break and Continue Statement/2_Example.cpp
@@ -1,28 +1,29 @@
 
+/*
+	Program in C++ to demonstrate the use of continue statement.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-int main()
-{
-	int next;
+int main() {
 
-	while (true)
-	{
-		cout << "Enter a number : ";
-		cin >> next;
-		if (next < 0) 
-		{
+	int number; // Declaring a variable of integer type
+
+	// Infinite loop, will terminate when user enters a negative number
+	while (true) {
+		cout << "Please enter a number : "; // Prompting user to enter a number
+		cin >> number; // Taking input from user
+		if (number < 0) {
 			break;
-		}
-		else
-		{     
+		} else {     
 			continue;
 		}
-	
 	}
-	cout << "Negative number so here we are" << endl;
+
+	cout << "Negative number so here we are" << endl; // Printing message, when user enters a negative number
 
 	_getch();
 	return 0;
