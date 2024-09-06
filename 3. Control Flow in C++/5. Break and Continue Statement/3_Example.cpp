1b7da5f35751a4039dac2d5573322772303342aa
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-06 20:36:55 +0500

    Added a C++ program to demonstrate the use of the break statement in generating a multiplication table. The program takes user input for a number and prints its multiplication table. If the input number is between 15 and 19, the program terminates the loop with a break statement and displays that tables are unavailable for these values. Otherwise, the table is printed up to 10. This example shows how to conditionally stop loops with a break.

--- 3. Control Flow in C++/5. Break and Continue Statement/3_Example.cpp
+++ 3. Control Flow in C++/5. Break and Continue Statement/3_Example.cpp
@@ -1,23 +1,37 @@
+
+/*
+    Program in C++ to demonstrate the use of break statement.
+*/
+
 #include<iostream> 
 #include<conio.h>
+
 using namespace std;
-int main()
-{
-    int a , b = 1, c;
-    cout << "Enter a Number to find its Table ==> ";
+
+int main() {
+
+    int a , b = 1, c; // Declaring three variables of integer type
+
+    cout << "Enter a Number to find its Table : ";
     cin >> a;
-    cout << "You asked for the table of : " << a << endl;
+    cout << "So, You have asked for the table of : " << a << endl;
+    
     do
     {
-        if (a >= 15 && a <= 19)
+        if (a >= 15 && a <= 19) {
+            // Break statement will terminate the loop when the 'a' is between 15 and 19
+            cout << "Tables not available";
             break;
-        c = a * b;
-        cout << a << "x" << b << "=" << c << endl;
-        b++;
+        } else {
+            // Else, the table will be printed
+            c = a * b;
+            cout << a << "x" << b << "=" << c << endl;
+            b++;
+        }
     } while (b <= 10);
     
+    cout << "Thank you for using our service" << endl; // Always printed
 
-    cout << "Tables not available";
     _getch();
     return 0;
 }
