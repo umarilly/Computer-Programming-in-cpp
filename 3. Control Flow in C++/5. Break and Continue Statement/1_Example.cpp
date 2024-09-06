6ca2c7b764c0d9ae4e769491c991884dfaa1b9d3
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-06 20:23:52 +0500

    Added a C++ program to demonstrate the use of the break statement in a do-while loop. The program prompts the user for input and uses the break statement to exit the loop if the user responds with 'Y' or 'y'. It illustrates how conditional logic can be used to terminate a loop prematurely. Clear variable names and comments are included to improve code readability.

--- 3. Control Flow in C++/5. Break and Continue Statement/1_Example.cpp
+++ 3. Control Flow in C++/5. Break and Continue Statement/1_Example.cpp
@@ -1,25 +1,31 @@
 
+/*
+	Program in C++ to demonstrate the use of break statement.
+*/
+
 #include<iostream>
 #include<conio.h>
+#include<string>
 
 using namespace std;
 
-int main()
-{
+int main() {
+
 	string ans; // We will discuss about strings later on 
 
 	do {
 		cout << "Would you help me ? ";
 		cin >> ans;
-	if (ans == "Y" || ans == "y") 
-	    break; // If user presses y, then remaining statements will terminate 
+	if (ans == "Y" || ans == "y") {
+		cout << "Thank you for your help!" << endl;
+		break; // If user presses y, then remaining statements will terminate 
+	}
+
 		cout << "Really ? ";
 		cin >> ans;
-	} 
-
-	while (ans != "Y" && ans != "y");
-	    cout << "Great!";
-
+	} while (ans != "Y" && ans != "y"); // If user presses y, then loop will terminate
+	
+	cout << "Great!";
 
 	_getch();
 	return 0;
