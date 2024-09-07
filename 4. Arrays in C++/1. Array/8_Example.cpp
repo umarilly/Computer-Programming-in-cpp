cceb999bbe871b16421ee85718bed3509c33ee58
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-07 21:51:00 +0500

    Added a C++ program to demonstrate the use of the const keyword with arrays. The program defines a constant integer size set to 20, which is used to declare an array of that size. It then allows the user to input values into the first 9 elements of the array and displays these values. This implementation highlights how to use constants in array declarations and manage array elements in C++.

--- 4. Arrays in C++/1. Array/8_Example.cpp
+++ 4. Arrays in C++/1. Array/8_Example.cpp
@@ -1,5 +1,7 @@
 
-// How to use const keyword
+/*
+	Program in C++ to demonstrate the use of const keyword in array.
+*/
 
 #include<iostream>
 #include<conio.h>
@@ -8,25 +10,22 @@ using namespace std;
 
 int main() {
 
-	const int size = 20;
-	int a[size];
-    
-	int i,j;
+	const int size = 20; // constant integer size of 20
+	int a[size]; // integer array "a" of size 20
 
-	for ( i = 0; i < 9; i++)
-	{
+	for (int i = 0; i < 9; i++) {
+		// Taking the input from the user
 		cout << "Value at [" << i << "] = ";
 		cin >> a[i];
 	}
 	
 	cout << "\n--------------------------\n\n";
 	
-	for (i = 0; i < 9; i++)
-	{
+	for (int i = 0; i < 9; i++) {
+		// Displaying the elements of the array
 		cout << "Value at [" << i << "] = " << a[i] << endl;
 	}
 	
 	_getch();
 	return 0;
-
 }
