486c088a19cc58035f634ecd8edf8df5a6155c23
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-07 21:49:02 +0500

    Updated C++ program to find and display the smallest element in an array. The program prompts the user to input the number of elements and their values, then identifies the smallest element by comparing each value to the initial element stored at index 0. The smallest value is displayed as the output. This update corrects the logic to properly identify the smallest element in the array.

--- 4. Arrays in C++/1. Array/7_Example.cpp
+++ 4. Arrays in C++/1. Array/7_Example.cpp
@@ -1,29 +1,38 @@
 
-// How to find Smallest Number
+/*
+    Program in C++ to find the largest element in an array.
+*/
 
 #include <iostream>
 #include <conio.h>
+
 using namespace std;
-int main()
-{
-    int a[10];
+
+int main() {
+
+    // Taking input from the user for the number of elements in the array
     int i, n;
     cout << "Enter the number of elements you want : ";
     cin >> n;
-    for (i = 0; i < n; i++)
-    {
+
+    int a[n]; // integer array "a" of size n
+
+    for (i = 0; i < n; i++) {
+        // Taking the input from the user
         cout << "Value at [" << i << "] = ";
         cin >> a[i];
     }
+
     cout << "\n--------------------------\n\n";
-    for (i = 0; i < n; i++)
-    {
-        if (a[i] < a[0])
-        {
+
+    for (i = 0; i < n; i++) {
+        // Storing the Smallest element of the array at the first index of the array
+        if (a[i] < a[0]) {
             a[0] = a[i];
         }
     }
-    cout << "Largest element is : " << a[0] << endl;
+
+    cout << "Largest element is : " << a[0] << endl; // Displaying the largest element of the array
 
     _getch();
     return 0;
