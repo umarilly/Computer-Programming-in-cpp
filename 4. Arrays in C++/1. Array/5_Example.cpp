5c4e6e9b0908fee20da306058a3f79241ee5a097
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-07 21:42:15 +0500

    Added a C++ program to demonstrate copying elements from one array to another and displaying the contents of the second array. The program first takes user input to populate an array a and then copies its elements to a second array b. Finally, it displays the elements of array b. This addition helps illustrate array copying and element display operations in C++.

--- 4. Arrays in C++/1. Array/5_Example.cpp
+++ 4. Arrays in C++/1. Array/5_Example.cpp
@@ -1,15 +1,19 @@
 
-// Copy 1 array into another
+/*
+	Program in C++ to copy the elements of one array into another array and then display the elements of the second array.
+*/
 
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
-int main()
-{
-	int a[5],b[5];
+
+int main() {
+
+	int a[5],b[5]; // integer array "a" of size 5 and "b" of size 5
 
 	for (int i = 0; i <= 4; i++) {
+		// Taking the input from the user
 		cout << "Value at [" << i << "] = ";
 		cin >> a[i];
 	}
@@ -17,12 +21,11 @@ int main()
 	cout << "\n--------------------------\n\n";
 
 	for (int i = 0; i <= 4; i++) {
+		// Copying the elements of array "a" into array "b"
 		b[i]= a[i];
-		cout << "Value at [" << i << "] = " << b[i] << endl;
-		
+		cout << "Value at [" << i << "] = " << b[i] << endl; // Displaying the elements of array "b"
 	}
 	
 	_getch();
 	return 0;
-
 }
