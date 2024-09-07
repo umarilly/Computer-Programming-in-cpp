41c2832794e4c6d66ff794cb4283eb372bc5818b
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-07 21:45:09 +0500

    Added a C++ program to find and display the largest element in an array. The program prompts the user to enter the number of elements and their values, then identifies the largest element by comparing each value to the first element. Finally, it outputs the largest element found. This implementation provides a practical example of array manipulation and basic comparison operations in C++.

--- 4. Arrays in C++/1. Array/6_Example.cpp
+++ 4. Arrays in C++/1. Array/6_Example.cpp
@@ -1,37 +1,39 @@
 
-// How to find the largest number in the array
+/*
+	Program in C++ to find the largest element in an array.
+*/
 
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-int main()
-{
-	int a[10];
+int main() {
 
+	// Taking input from the user for the number of elements in the array
 	int n;
 	cout << "Enter the number of elements you want : ";
 	cin >> n;
 
-	for ( int i = 0; i < n; i++)
-	{
+	int a[n]; // integer array "a" of size n
+
+	for ( int i = 0; i < n; i++) {
+		// Taking the input from the user
 		cout << "Value at [" << i << "] = ";
 		cin >> a[i];
 	}
 
 	cout << "\n--------------------------\n\n";
     
-	for ( int i = 0; i < n; i++)
-	{
-		if (a[i] > a[0])
-		{
+	for ( int i = 0; i < n; i++) {
+		// Displaying the elements of the array
+		if (a[i] > a[0]) {
 			a[0] = a[i];
 		}
 	}
-	cout << "Largest element is : " << a[0] << endl;
+
+	cout << "Largest element is : " << a[0] << endl; // Displaying the largest element of the array
 	
 	_getch();
 	return 0;
-
 }
