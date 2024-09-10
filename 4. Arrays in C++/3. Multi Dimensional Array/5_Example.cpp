3492c4e9fe137c0c3f9668691eafc7398af7e2e3
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-10 12:26:10 +0500

    Added a C++ program that declares two 2D arrays of size 3x3, takes input from the user, and computes their addition. The program prompts the user to input values for both arrays and then calculates the sum element-wise, storing the result in a third array. Finally, it displays the resulting 2D array after the addition. This commit enhances the project by introducing array arithmetic operations, providing a clear example of how to perform addition on multi-dimensional arrays in C++.

--- 4. Arrays in C++/3. Multi Dimensional Array/5_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/5_Example.cpp
@@ -1,45 +1,56 @@
+
+/*
+	Program in C++ to declare two 2D arrays of size 3x3 and take input from user and display the addition of two 2D arrays.
+*/
+
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
+
 using namespace std;
-int main()
-{
+
+int main() {
+
 	int abc[3][3],xyz[3][3],add[3][3];
 	int i, j;
-	cout << "Enter values of First 2-D array : \n";   
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	cout << "Enter values of First 2-D array : \n";  
+	
+	// Taking input from user for first 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cin >> abc[i][j];
 		}
 	}
-	cout << endl;
-	cout << "Enter values of Second 2-D array : \n";
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	cout << "\nEnter values of Second 2-D array : \n";
+
+	// Taking input from user for second 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cin >> xyz[i][j];
 		}
 	}
+
 	cout << endl;
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	// Addition of two 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			add[i][j] = abc[i][j] + xyz[i][j];
 		}
 	}
+
 	cout << "Addition of First and Second 2-D array is : \n";
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+	
+	// Displaying the addition of two 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cout << add[i][j] << setw(5);
 		}
 		cout << "\n";
 	}
+
 	_getch();
 	return 0;
 }
