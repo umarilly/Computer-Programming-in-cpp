7ae39c44fbaf026d5b63ad4975e8ff98ec9cdfbb
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-10 12:12:48 +0500

    Added a new C++ program demonstrating the declaration of a 2D array of size 3x3. The program includes basic syntax for declaring a 2D array using the int data type and initializes a 3x3 array named abc. This serves as a foundational example for understanding multi-dimensional arrays in C++. Additionally, _getch() is used to pause the console before termination. This commit enhances the educational content for array declaration in the repository.

--- 4. Arrays in C++/3. Multi Dimensional Array/1_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/1_Example.cpp
@@ -1,11 +1,16 @@
 
+/*
+	Program in C++ to declare a 2D array of size 3x3.
+*/
 
 #include<iostream>
 #include<conio.h>
+
 using namespace std;
-int main()
-{
-	int abc[3][3];  // Declaration
+
+int main() {
+
+	int abc[3][3];  // Declaration of 2D array of size 3x3
 	
 	_getch();
 	return 0;
