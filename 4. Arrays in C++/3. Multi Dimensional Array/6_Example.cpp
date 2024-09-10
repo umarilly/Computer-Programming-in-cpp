e8d8d1b2e44d8a2907a8b31c1caf14d3f19b283e
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-10 12:29:08 +0500

    Added a C++ program that declares two 2D arrays of size 3x3, takes user input, and computes the subtraction of the two arrays. The program prompts the user to input values for both arrays, then calculates the element-wise subtraction, storing the result in a third array. Finally, it displays the result of the subtraction. This commit introduces array subtraction operations, demonstrating how to handle arithmetic between multi-dimensional arrays in C++.

--- 4. Arrays in C++/3. Multi Dimensional Array/6_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/6_Example.cpp
@@ -1,46 +1,56 @@
 
+/*
+	Program in C++ to declare two 2D arrays of size 3x3 and take input from user and display the subtraction of two 2D arrays.
+*/
+
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
+
 using namespace std;
-int main()
-{
+
+int main() {
+
 	int abc[3][3],xyz[3][3],sub[3][3];
 	int i, j;
+
 	cout << "Enter values of First 2-D array : \n";   
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+	
+	// Taking input from user for first 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cin >> abc[i][j];
 		}
 	}
-	cout << endl;
-	cout << "Enter values of Second 2-D array : \n";
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	cout << "\nEnter values of Second 2-D array : \n";
+
+	// Taking input from user for second 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cin >> xyz[i][j];
 		}
 	}
+	
 	cout << endl;
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	// Subtraction of two 2D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			sub[i][j] = abc[i][j] - xyz[i][j];
 		}
 	}
+
 	cout << "Subtraction of First and Second 2-D array is : \n";
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	// Displaying the subtraction of two 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cout << sub[i][j] << setw(5);
 		}
 		cout << "\n";
 	}
+
 	_getch();
 	return 0;
 }
