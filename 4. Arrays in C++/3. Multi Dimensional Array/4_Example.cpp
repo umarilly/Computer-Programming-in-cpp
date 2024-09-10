6a41ece95111cd6558ab58f20dd54fbe0ed26c04
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-10 12:22:46 +0500

    Added a C++ program that declares a 2D array, takes user input, and displays the values of the array. The program defines a 2x2 array abc and uses nested loops to input values from the user and then print them. This example demonstrates how to interact with a 2D array by accepting input and displaying the stored values, providing a practical use case for user-driven data entry in multi-dimensional arrays. This commit enhances the project by introducing user interaction for array manipulation.

--- 4. Arrays in C++/3. Multi Dimensional Array/4_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/4_Example.cpp
@@ -1,26 +1,32 @@
 
+/*
+	Program in C++ to declear a 2D array and take input from user and display the value of 2-D array.
+*/
+
 #include <iostream>
 #include <conio.h>
 
 using namespace std;
 
-int main()
-{
-	int abc[2][2];
-	int i, j;
+int main() {
+
+	int abc[2][2]; // 2-D array declearation
+	int i, j; // loop variable
+
 	cout << "Enter value of 2-D array : \n" ;
-	for ( i = 0; i < 2; i++)
-	{
-		for (j = 0; j <2;j++)
-		{
+
+	for ( i = 0; i < 2; i++) {
+		// loop for column
+		for (j = 0; j <2;j++) {
+			// loop for row
 			cin>> abc[i][j];
 		}
 	}
 	cout << "The value of 2-D array are : \n" ;
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
+
+	// Nested for loop for printing of values of 2D array
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
 			cout << abc[i][j]<<"  ";
 		}
 		cout << "\n";
