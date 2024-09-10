a8c6c0d7f36ac249b58190341f3ab83cc73ac448
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-10 12:33:26 +0500

    Added a C++ program that calculates the sum of each row in a 2D array. The program prompts the user to input values for a 3x3 matrix, and it then computes and displays the sum of elements row by row. This commit demonstrates handling row-wise operations in a 2D array and introduces array traversal techniques using nested loops in C++.

--- 4. Arrays in C++/3. Multi Dimensional Array/7_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/7_Example.cpp
@@ -1,4 +1,8 @@
 
+/*
+	Program in C++ to sum the rows of a 2D array.
+*/
+
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
@@ -10,22 +14,22 @@ int main()
 	int abc[3][3];
 	int i, j;
 	int sum = 0;
-	cout << "Enter values of First 2-D array : \n";   
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
+
+	cout << "Enter values of First 2-D array : \n";  
+	
+	// Taking input from user for first 2-D array
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			cin >> abc[i][j];
 		}
 	}
 	
 	cout << endl;
 	
-	for (i = 0; i < 3; i++)
-	{
+	// Displaying the values of 2-D array
+	for (i = 0; i < 3; i++) {
 		cout << endl;
-		for (j = 0; j < 3; j++)
-		{
+		for (j = 0; j < 3; j++) {
 			cout << abc[i][j] << " ";
 			sum = sum + abc[i][j];
 		}
@@ -36,7 +40,6 @@ int main()
 	_getch();
 	return 0;
 }
-
 
 /*
 #include<iostream>
