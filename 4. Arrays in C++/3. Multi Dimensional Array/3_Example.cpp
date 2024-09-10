5ca23a8b17f4ff143d13635a410ef79037ce1879
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-10 12:19:56 +0500

    Added a C++ program that declares, initializes, and prints the values of a 2D array using a nested for loop. The program defines a 3x3 array abc and prints its values row by row. This example demonstrates how to access and display individual elements of a 2D array efficiently using loops. It serves as a practical illustration of working with multi-dimensional arrays in C++, providing clarity on array traversal and element access.

--- 4. Arrays in C++/3. Multi Dimensional Array/3_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/3_Example.cpp
@@ -1,22 +1,27 @@
 
+/*
+    Program in C++ to declare and initialize a 2D array of size 3x3 and print the values of the array using nested for loop.
+*/
+
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-int main()
-{
+int main() {
+
     int abc[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // Declaration & Initialization
     int i, j;
     cout << "The value of 2-D array are : \n"; // Printing of Initialized Vales
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            cout << abc[i][j] << "  ";
+    for (i = 0; i < 3; i++) {
+        // Nested for loop for printing of values of 2D array
+        for (j = 0; j < 3; j++) {
+            cout << abc[i][j] << "  "; // Printing of values of 2D array
         }
         cout << "\n";
     }
 
+    _getch();
     return 0;
 }
