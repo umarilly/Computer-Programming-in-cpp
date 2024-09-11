c4481744afe3ec5915a1d22a70f908f5c7b8cac0
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-11 10:02:35 +0500

    Added a C++ program to implement Linear Search for finding the index of an element in a user-defined array of size 10. The program allows the user to input the array elements and then prompts for a number to search within the array. It uses a loop to find the index of the specified number and displays the result. This update demonstrates the basic usage of arrays, input handling, and linear search logic in C++ for educational purposes.

--- 4. Arrays in C++/5. Searching in Array/1. Linear Search/2_Example.cpp
+++ 4. Arrays in C++/5. Searching in Array/1. Linear Search/2_Example.cpp
@@ -1,37 +1,45 @@
+
+/*
+	Program in C++ to search an the index of element in an array using Linear Search.
+*/
+
 #include<iostream>
 #include<conio.h>
-using namespace std;
-int main()
-{
-	int x, num;
-	const int size = 10;
-	int abc[size];
 
+using namespace std;
+
+int main() {
+
+	int x, num; // x is for index number and num is for number to search
+	const int size = 10; // Declaring the size of the array
+	int abc[size]; // Declaring an array of size 10
+
+	// Taking input from user to search the number
 	cout << "Enter the Elements of the Array :- \n\n";
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < size; i++) {
 		cout << "The Element at index [" << i << "] is : ";
 		cin >> abc[i];
 	}
-	cout << "\n";
-	cout << "The Elements you entered in the Array are :- \n";
-	for (int i = 0; i < size; i++)
-	{
+
+	// Displaying the elements of the array
+	cout << "\nThe Elements you entered in the Array are :- \n";
+	for (int i = 0; i < size; i++) {
 		cout << abc[i] << "  ";
 	}
-	cout << "\n\n";
-	cout << "Entered the number you want to find in Array :- \n";
+
+	// Taking input from user to search the index of the number
+	cout << "\n\nEntered the number you want to find in Array :- \n";
 	cin >> num;
 
-	for (int index_no = 0; index_no < size; index_no++)
-	{
-		if (num == abc[index_no])
-		{
+	for (int index_no = 0; index_no < size; index_no++) {
+		if (num == abc[index_no]) {
 			x = index_no;
 		}
 	}
-	cout << "\n";
-	cout << "\nThe Entered Number is found on index number : " << x << endl;
+
+	// Displaying the index number of the entered number
+	cout << "\n\nThe Entered Number is found on index number : " << x << endl;
+
 	_getch();
 	return 0;
 }
