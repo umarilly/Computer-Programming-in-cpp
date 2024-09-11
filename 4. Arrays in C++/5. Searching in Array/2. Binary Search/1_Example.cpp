a9e162d2763d560ec58f77e491cc991ac12d0d8c
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-11 10:07:11 +0500

    Added a C++ program to implement Binary Search for locating the index of a specific number in a user-defined array. The program allows the user to input array elements, which are then searched using the Binary Search algorithm. It calculates the middle element, compares it with the target number, and adjusts the search range accordingly. The result is displayed, showing either the index of the found number or a message indicating the number is not present. This update demonstrates efficient search techniques and array handling in C++.

--- 4. Arrays in C++/5. Searching in Array/2. Binary Search/1_Example.cpp
+++ 4. Arrays in C++/5. Searching in Array/2. Binary Search/1_Example.cpp
@@ -1,45 +1,55 @@
 
+/*
+	Program in C++ to search the number at particular index in an array using Binary Search.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-int main()
-{
+int main() {
+
 	const int size = 10;
 	int abc[size];
 	int first = 0, mid, last = 9;
 	int number;
 
+	// Taking input from user to search the number
 	cout << "Enter the Elements of the Array :- \n\n";
 	for (int i = 0; i < size; i++) {
 		cout << "The Element at index [" << i << "] is : ";
 		cin >> abc[i];
 	}
-	cout << "\n";
-	cout << "The Elements you entered in the Array are :- \n";
+
+	// Displaying the elements of the array
+	cout << "\nThe Elements you entered in the Array are :- \n";
 	for (int i = 0; i < size; i++) {
 		cout << abc[i] << "  ";
 	}
-	cout << "\n\n";
-	cout << "Entered the number you want to find in Array :- \n";
+
+	// Taking input from user to search the index of the number
+	cout << "\n\nEntered the number you want to find in Array :- \n";
 	cin >> number;
 
+	// Sorting the array
 	while (first <= last) {
+		// Calculating the middle element of the array
 		mid = (first + last) / 2;
-
 		if (number == abc[mid]) {
+			// Displaying the index number of the entered number
 			cout << "\nThe Number is found at index number : " << mid << endl;
 			break;
-		}
-		else if (number > abc[mid]) {
+		} else if (number > abc[mid]) {
+			// If the number is greater than the middle element
             first = (mid + 1);
-        }
-		else if (number < abc[mid]) {
+        } else if (number < abc[mid]) {
+			// If the number is smaller than the middle element
             last = (mid - 1);
         }
 	}
 
+	// If the number is not found
 	if (number < abc[mid] || number > abc[mid]) {
 		cout << "\nNumber not found " << endl;
 	}
