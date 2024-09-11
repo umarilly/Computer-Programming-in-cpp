358f706bae5da3dac08576d2aac73964cf84f354
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-11 09:54:14 +0500

    Implemented a C++ program that declares a 2D array of size 3x4 containing strings, representing various categories like animals, tech products, and brands. The matrix is displayed using nested loops, printing each element in a tabular format. The code includes structured comments for clarity and uses standard C++ libraries for input/output operations. This update demonstrates fundamental matrix handling and iteration using loops in C++.

--- 4. Arrays in C++/3. Multi Dimensional Array/9_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/9_Example.cpp
@@ -1,22 +1,23 @@
 
+/*
+	Program in C++ to declare a 2D array of size 3x4 and display the elements of the matrix.
+*/
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
 
 using namespace std;
 
-int main()
-{
+int main() {
+
+	// Declaring a 2D array of size 3x4
 	string xyz[3][4] = { {"Bird","Animal","Fish","Monstar"},{"iphone","Ultra","Max","Pro"},{"Samsung","Galaxy","Hot","Note"} };
-
-	int i, j;
-
-	cout << "The Martix is : \n";
+	int i, j; // i is for row and j is for column
+	cout << "The Martix is : \n"; // Displaying the elements of the matrix
     
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 3; j++)
-		{
+	// Loop to display the elements of the matrix
+	for (i = 0; i <= 2; i++) {
+		for (j = 0; j <= 3; j++) {
 			cout << xyz[i][j] << "\t"<<" ";
 		}
 		cout << endl;
