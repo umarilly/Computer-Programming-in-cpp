cbfa2c70403a2748d99ba472e46081db75c58117
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-11 09:59:48 +0500

    Added a C++ program to implement linear search for finding the index of an element in a predefined array of size 20. The user inputs a number, and the program searches for its index using a simple loop. If the number is found, the corresponding index is displayed. This update demonstrates the basic concept of linear search and provides a functional example of array traversal in C++.

--- 4. Arrays in C++/5. Searching in Array/1. Linear Search/1_Example.cpp
+++ 4. Arrays in C++/5. Searching in Array/1. Linear Search/1_Example.cpp
@@ -1,25 +1,32 @@
 
+/*
+	Program in C++ to search the index of element in an array using linear search.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-int main()
-{
-	int x, n;
+int main() {
+
+	int x, n; // x is for index number and n is for number to search
+	// Declaring an array of size 20
 	int a[20] = { 34, 56, 54, 45, 32, 98, 70, 81, 93, 14, 41, 52, 63, 74, 35, 26, 57, 68, 99, 27 };
+	
+	// Taking input from user to search the number
 	cout << "Enter the number to search its position in this array : ";
 	cin >> n;
 
-	for (int index_no = 0; index_no < 20; index_no++)
-	{
-		if (n == a[index_no])
-		{
+	// Searching the index of the number in the array
+	for (int index_no = 0; index_no < 20; index_no++) {
+		if (n == a[index_no]) {
 			x = index_no;
 		}
 	}
-	cout << "\n";
-	cout << "The index number or position of the entered number is : " << x << endl;
+
+	cout << "\nThe index number or position of the entered number is : " << x << endl;
+
 	_getch();
 	return 0;
 }
