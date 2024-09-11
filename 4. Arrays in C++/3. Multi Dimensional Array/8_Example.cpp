fe9525aa99d0c39dfcb04936abda10cf14105cd9
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-11 09:52:08 +0500

    Added program to declare and display elements of a 3x4 2D array in C++. The matrix is initialized with character values, and a nested loop is used to print each element in a tabular format. Included appropriate headers, comments, and structure for better readability. This program demonstrates basic usage of 2D arrays in C++ and proper matrix traversal with loops.

--- 4. Arrays in C++/3. Multi Dimensional Array/8_Example.cpp
+++ 4. Arrays in C++/3. Multi Dimensional Array/8_Example.cpp
@@ -1,23 +1,30 @@
 
+/*
+	Program in C++ to declare a 2D array of size 3x4 and display the elements of the matrix.
+*/
+
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
 
 using namespace std;
 
-int main()
-{
+int main() {
+
+	// Declaring a 2D array of size 3x4
 	char xyz[3][4] = { {'f','s','t','m'},{'g','r','u','s'},{'m','q','a','w'} };
-	int i, j;
-	cout << "The Martix is : \n";
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 3; j++)
-		{
+	int i, j; // i is for row and j is for column
+
+	cout << "The Martix is : \n"; // Displaying the elements of the matrix
+
+	// Loop to display the elements of the matrix
+	for (i = 0; i <= 2; i++) {
+		for (j = 0; j <= 3; j++) {
 			cout << xyz[i][j] << "\t"<<" ";
 		}
 		cout << endl;
 	}
+
 	_getch();
 	return 0;
 }
