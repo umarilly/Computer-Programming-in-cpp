a1ecb7749d6b58f7a7a1c515c157be0a4b0c545a
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-14 19:31:26 +0500

    Added a C++ program that demonstrates the use of a simple function to display a list of operations and perform an action based on user input. The program defines a function called 'simpleFunction' that lists available operations. After the list is displayed, the user selects an option, and a switch case is used to determine the corresponding action: outputting, inputting, or processing. This commit showcases basic user interaction, function usage, and switch case control structures in C++.

--- 5. Functions in C++/1. Basics of Functions in C++/2_Example.cpp
+++ 5. Functions in C++/1. Basics of Functions in C++/2_Example.cpp
@@ -1,24 +1,30 @@
 
+/*
+    Program in C++ to create a simple function to display the list of operations and perform the operation based on the user input.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
 /* No function Prototype, if function defination is above the main(), then there is no need of function Prototype */
+
 void simpleFunction() {
-	cout << "Press 1 for Output : " <<endl ;
+	cout << "Press 1 for Output : " << endl ;
 	cout << "Press 2 for input : " << endl;
 	cout << "Press 3 for process : " << endl;
 }
 
 int main() {
 
-	simpleFunction();
+	simpleFunction(); // Function Call
 	
 	int operation;
 	cout << "Enter from list : ";
 	cin >> operation;
 
+    // Switch case to perform the operation based on the user input
 	switch (operation) {
         case 1: {
             cout << "Outputting ... " << endl;
