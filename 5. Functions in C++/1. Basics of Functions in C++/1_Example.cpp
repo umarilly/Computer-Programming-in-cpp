a8ef45ed8cfeb59daee16fb6986437b7eebabeec
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-14 19:26:31 +0500

    Added a program in C++ to find the sum of two numbers using a function. The program takes two integers as input from the user and passes them to a function named 'sum', which calculates and returns the sum of the two numbers. The result is then displayed to the user. This commit demonstrates the use of function prototypes, function definition, and function invocation in C++, providing a clear example of modular programming for basic arithmetic operations.

--- 5. Functions in C++/1. Basics of Functions in C++/1_Example.cpp
+++ 5. Functions in C++/1. Basics of Functions in C++/1_Example.cpp
@@ -1,25 +1,32 @@
 
+/*
+	Program in C++ to find the sum of two numbers using function.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
-int sum(int a, int b); // Function Prototype
+// Function Prototype
+int sum(int a, int b); 
 
 int main() {
+
 	int num1, num2;
 	cout << "Enter the first numbers : ";
 	cin >> num1;
     cout << "Enter the second numbers : ";
 	cin >> num2;
 
+	// Function Call or Function Invocation or Function Execution
 	cout << "The sum of two numbers are : " << sum(num1, num2) << endl;
 
 	_getch();
 	return 0;
 }
 
-/* Function Defination */
+//Function Defination
 int sum(int a, int b) {
     return (a + b);
 }
