c88816d7df562b8195dea4eaa37369ffac80b3be
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-15 19:32:30 +0500

    Added a program in C++ that demonstrates the use of call by value. The program defines a function sumFunction that takes two integers, modifies their values locally, and returns their sum. The values of the variables a and b in the main function remain unchanged, showcasing the behaviour of call by value. The program includes output to display the values before and after the function call and prints the sum of the modified values. This helps to illustrate how changes within the function do not affect the original arguments passed from the main function.

--- 5. Functions in C++/2. Call or Pass by Value/1_Example.cpp
+++ 5. Functions in C++/2. Call or Pass by Value/1_Example.cpp
@@ -1,4 +1,9 @@
 
+/*
+	Program in C++ to demonstrate the use of call by value.
+*/
+
+
 #include<iostream>
 #include<conio.h>
 
@@ -15,11 +20,15 @@ int sumFunction(int x, int  y) {
 }
 int main() {
     
-	int a = 15, b = 20;
+	// a and b are local to main function
+	int a = 15, b = 20; 
+
 	cout << "\na and b in main are \n" << a << " & " << b << endl;
 
+	// a and b are passed by value
 	int c = sumFunction(a, b);
-	cout << "\n&, The sum is : " << c << endl;
+
+	cout << "\n&, The sum is : " << c << endl; // This line will print display the output
 
 	_getch();
 	return 0;
