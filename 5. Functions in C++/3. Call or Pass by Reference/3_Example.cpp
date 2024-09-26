4bd0a6ce2ed0bb1c4fd2460a2223777ea286cce4
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-26 23:27:52 +0500

    Added a C++ program to calculate the sum and average of two numbers using call by reference. The sum_avg function takes two input numbers and calculates both the sum and the average, modifying the referenced variables accordingly. In the main function, the user is prompted to enter two numbers, and the function is called to compute the results. The program then prints the sum and average of the input numbers, demonstrating how call by reference updates values in the calling environment.

--- 5. Functions in C++/3. Call or Pass by Reference/3_Example.cpp
+++ 5. Functions in C++/3. Call or Pass by Reference/3_Example.cpp
@@ -1,20 +1,28 @@
+
+/*
+	Program in C++ to find the sum and average of two numbers using call by reference.
+*/
+
 #include<iostream>
 #include<conio.h>
+
 using namespace std;
-void sum_avg (int & i , int & j , int & sum , int & avg)
-{
+
+// Function to calculate sum and average of two numbers
+void sum_avg (int &i , int &j , int &sum , int &avg) {
 	sum = i + j;
 	avg = sum / 2;
 }
-int main()
-{
+
+int main() {
+
 	int a, b;
 	int sum = 0, avg = 0;
 
 	cout << "Enter two numbers :- \n";
 	cin >> a >> b;
 
-	sum_avg( a, b, sum, avg);
+	sum_avg( a, b, sum, avg); // Calling function by reference
 
 	cout << "The sum is : " << sum << endl;
 	cout << "The avg is : " << avg << endl;
