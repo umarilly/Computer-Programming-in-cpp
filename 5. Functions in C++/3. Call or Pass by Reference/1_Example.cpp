f71d5ae217757bf9dbbb0dc8dff599b5343744a7
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-26 23:23:51 +0500

    Added a C++ program demonstrating the concept of call by reference, where two integer variables are passed to a function that modifies their values. The sum function adds 5 to both integers and returns their sum, while also printing the updated values within the function. In the main function, the updated values of the variables are displayed after the function call, showing that the changes made inside the function persist in the calling environment.

--- 5. Functions in C++/3. Call or Pass by Reference/1_Example.cpp
+++ 5. Functions in C++/3. Call or Pass by Reference/1_Example.cpp
@@ -1,26 +1,29 @@
 
+/*
+	Program in C++ to demonstrate call by reference.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
+// Function to calculate sum of two numbers
 int sum(int & x, int & y) {
-	
     x = x + 5;
 	y = y + 5;
 	cout << "\na and b in function are \n" << x << " & " << y << endl;
 	return x + y;
-
 }
 
 int main() {
     
 	int a = 10, b = 15;
-	int c = sum(a, b);
+	int c = sum(a, b); // Calling function by reference
 	
-	cout << "\nSum is : " << c << endl;
-
-	cout << "\na and b in main are \n" << a << " & " << b << endl;
+	cout << "\nSum is : " << c << endl; // This will print the sum
+	// This will print the updated values of a and b
+	cout << "\na and b in main are \n" << a << " & " << b << endl; 
 
 	_getch();
 	return 0;
