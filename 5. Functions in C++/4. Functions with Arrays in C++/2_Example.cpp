0211ae249ed184c963ba0feb0e988541a9c89963
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-28 22:26:03 +0500

    Added a C++ program to find the maximum value in an array using a function. The display function takes an integer array and its size as parameters to input and print the array elements. Inside the function, a loop is used to input values into the array, and then another loop is used to display the elements. The maximum value is determined by iterating through the array and comparing elements, and the result is displayed.

--- 5. Functions in C++/4. Functions with Arrays in C++/2_Example.cpp
+++ 5. Functions in C++/4. Functions with Arrays in C++/2_Example.cpp
@@ -1,32 +1,39 @@
 
-// Finding the maximum value in the array using function
+/*
+F   inding the maximum value in the array using function
+*/
 
 #include <iostream>
 #include <conio.h>
 
 using namespace std;
 
+// Function to display the elements of the array
 void display(int marks[], const int size) {
     cout << "Enter the elements of the array :\n";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++) {
         cin >> marks[i];
+    }
 
     cout << "The elements of the array are:\n";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++) {
         cout << marks[i] << " ";
+    }
 
     int max_value = marks[0];
-    for (int i = 1; i < size; i++)
-        if (marks[i] > max_value)
+    for (int i = 1; i < size; i++) {
+        if (marks[i] > max_value) {
             max_value = marks[i];
-
-    cout << "\nThe maximum value in the array is: " << max_value << endl;
+        }
+    }
+    // Displaying the maximum value in the array 
+    cout << "\nThe maximum value in the array is : " << max_value << endl;
 }
 
 int main() {
-    const int size = 5;
-    int marks[size];
-    display(marks, size);
+    const int size = 5; // Size of the array
+    int marks[size]; // Array declaration
+    display(marks, size); // Passing the array to the function
     
     _getch();
     return 0;
