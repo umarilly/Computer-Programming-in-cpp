2650caef5227344ee571fcd8682d46d1d8c6a146
Muhammad Umar <Muhammadumar695@gmail.com>
2024-09-28 22:22:51 +0500

    Implemented a C++ program to demonstrate the use of functions with arrays. Added a function named display that takes an integer array and its size as parameters to print the array elements. The array marks is initialized with predefined values and passed to the display function from the main function. Used sizeof operator to calculate the array size. Ensured proper inclusion of headers and handled array output through a loop.

--- 5. Functions in C++/4. Functions with Arrays in C++/1_Example.cpp
+++ 5. Functions in C++/4. Functions with Arrays in C++/1_Example.cpp
@@ -1,20 +1,27 @@
 
+/*
+	Program in C++ to demonstrate the use of functions with arrays.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
+// Function to display the elements of the array
 void display (int marks[], int size) {
 	cout << "The Elemets of the Array are :- \n";
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++) {
 		cout << marks[i] << endl;
+	}
 }
 
 int main() {
 
 	int marks[] = { 3,5,7,9,5 };
     int size = sizeof(marks)/sizeof(marks[0]);
-	display(marks,size);
+
+	display(marks,size); // Passing the array to the function
 	
 	_getch();
 	return 0;
