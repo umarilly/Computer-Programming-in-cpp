a3b09862c80671fab5df50cb7654cf387cc44eba
Muhammad Umar <Muhammadumar695@gmail.com>
2024-11-12 23:36:16 +0500

    feat: updated the program to add comments in it for better understanding

--- 5. Functions in C++/1. Basics of Functions in C++/3_Example.cpp
+++ 5. Functions in C++/1. Basics of Functions in C++/3_Example.cpp
@@ -1,11 +1,15 @@
 
+/*
+	Program in C++ to print the table of 2 using function.
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
 int table(int a, int b, int c) {
-
+	// for loop to print the table of 2
 	for (a = 2; b <= 10; b++) {
 		c = a * b;
 		cout << a << "*" << b << "=" << c << endl;
@@ -16,9 +20,11 @@ int table(int a, int b, int c) {
 
 int main() {
 
-	int i=2, j=1, k=0;
+	// Declaration of variables
+	int i=2, j=1, k=0; 
 
 	cout << "Table of Two :- " << endl;
+	// Function call and passing the values
 	table(i, j, k);
 
 	_getch();
