2e928df5e1ff8f0f74049353e9cc3f4e4128a628
Muhammad Umar <Muhammadumar695@gmail.com>
2024-11-14 23:55:05 +0500

    feat: implemented a program to print the table of any number according to user input.

--- 5. Functions in C++/1. Basics of Functions in C++/4_Example.cpp
+++ 5. Functions in C++/1. Basics of Functions in C++/4_Example.cpp
@@ -1,24 +1,27 @@
 
+/*
+	Program in C++ to print the table of any number
+*/
+
 #include<iostream>
 #include<conio.h>
 
 using namespace std;
 
+// Function with three parameters to print the table of a number
 int table(int a, int b, int c) {
-	
     for (a; b <= 10; b++) {
-		c = a * b;
+		c = a * b; // Multiplication of two numbers
 		cout << a << "*" << b << "=" << c << endl;
 	}
-
 	return c;
 }
 
 int main() {
-
 	int i, j=1, k=0;
 	cout << "Enter the number whose table you want : ";
 	cin >> i;
+	// Function call to print the table of a number
 	table(i, j, k);
 
 	_getch();
